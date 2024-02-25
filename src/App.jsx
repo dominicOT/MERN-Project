a830f52cbbb6e4ffdd0b7e7a2a3dcb0bbfe58f03
AceDev <dominicoladapotonade@gmail.com>
2024-02-25 17:37:12 +0000

    fucking COMMIT ALREADY, HAULASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+// import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+
+
+import Header from './components/Header';
+
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" elements={<Home />} />
+    <Route path="/sign-up" elements={<SignUp />} />
+    <Route path="/sign-in" elements={<SignIn />} />
+    <Route path="/profile" elements={<Profile />} />
+    <Route path="/about" elements={<About />} />
+  </Routes>
+  </BrowserRouter>
 }
