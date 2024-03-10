d6249b8b81b90aa8d86d4504069d2596888c2582
AceDev <dominicoladapotonade@gmail.com>
2024-03-10 06:54:45 +0000

    insomnia.rest, bcryptjs

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,16 @@
-export const signup = (req, res) =>  {
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
+
+export const signup = async (req, res) =>  {
+
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    
+    const newUser = new User({ username, email, hashedPassword });
+    await newUser.save()
+    .then(() => {
+        res.status(201).json({ message: "User created successfully" });
+    })
 
 };
