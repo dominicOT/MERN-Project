559d07874dfc8e2849ea8cff1c32558fc2ab970e
AceDev <dominicoladapotonade@gmail.com>
2024-03-11 13:45:08 +0000

    signup api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,10 +7,15 @@ export const signup = async (req, res) =>  {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     
-    const newUser = new User({ username, email, hashedPassword });
-    await newUser.save()
-    .then(() => {
-        res.status(201).json({ message: "User created successfully" });
-    })
+    const newUser = new User({ username, email, password: hashedPassword });
 
+    try {
+        await newUser.save()
+        .then(() => {
+            res.status(201).json({ message: "User created successfully" });
+        })
+    } 
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
