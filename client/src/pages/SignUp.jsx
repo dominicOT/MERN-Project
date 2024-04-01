c0d8a6c10404f09155f6852bfd2773f6dc5b7cb3
AceDev <dominicoladapotonade@gmail.com>
2024-04-01 11:36:26 +0100

    sign-up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,12 @@
 export default function SignUp() {
   return (
-    <div>
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input type="text" placeholder='Username' className="border p-3 rounded-lg" id="username"/>
+        <input type="email" placeholder='Email' className="border p-3 rounded-lg" id="email"/>
+        <input type="password" placeholder='Password' className="border p-3 rounded-lg" id="password"/>
+      </form>
     </div>
   )
 }
