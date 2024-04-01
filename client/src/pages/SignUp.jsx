23a4c5085b65bd0b5aef0398986e7aca3c64109a
AceDev <dominicoladapotonade@gmail.com>
2024-04-01 13:32:12 +0100

    sign-up page complete

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
 export default function SignUp() {
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -6,7 +9,15 @@ export default function SignUp() {
         <input type="text" placeholder='Username' className="border p-3 rounded-lg" id="username"/>
         <input type="email" placeholder='Email' className="border p-3 rounded-lg" id="email"/>
         <input type="password" placeholder='Password' className="border p-3 rounded-lg" id="password"/>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:95 disabled:opacity-80">Sign Up</button>
       </form>
+
+      <div className='flex gap-2 mt-5'>
+        <p>Already have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>Login</span>
+        </Link>
+      </div>
     </div>
   )
 }
